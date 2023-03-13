89826d657c63629783f696c9fcfc8b104ad78c73
sophie <sofiabayol@mail.ru>
2023-03-13 03:38:23 +0300

    add files

---
+++ lab3.2.py
@@ -0,0 +1,7 @@
+prog = []
+while True:
+    ele = input()
+    if ele == "stop":
+        break
+    prog.append(ele)
+print(prog)
---
+++ laba3.4.py
@@ -0,0 +1,18 @@
+print('Математика для детей: ')
+k=0
+x=0
+y=0
+for i in range(1,8):
+    for j in range(1,8):
+        print( i,"+", j,"= ",end="")
+        res = int(input())
+        k = i + j
+        if res == k:
+            print('Правильно!')
+            x+=1
+        else:
+            print('Неверно.')
+            y+=1
+            if y==3:
+                print('Игра окончена. Правильных ответов:', x)
+
